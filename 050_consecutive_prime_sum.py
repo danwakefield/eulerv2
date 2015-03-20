6ba13158216cc0b009c2c84f3da30855a91cc122
Daniel Wakefield <daniel.wakefield@hp.com>
2015-03-20 18:01:35 +0000

    Changed code. Still doesnt break properly.

--- 050_consecutive_prime_sum.py
+++ 050_consecutive_prime_sum.py
@@ -3,18 +3,18 @@
 """
 https://projecteuler.net/problem=50
 
-The prime 41, can be written as the sum of six consecutive primes: 41
-= 2 + 3 + 5 + 7 + 11 + 13 This is the longest sum of consecutive
-primes that adds to a prime below one-hundred. The longest sum of
-consecutive primes below one-thousand that adds to a prime, contains
-21 terms, and is equal to 953. Which prime, below one-million, can be
-written as the sum of the most consecutive primes?
+The prime 41, can be written as the sum of six consecutive primes:
+    41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below
+one-hundred. The longest sum of consecutive primes below one-thousand that adds
+to a prime, contains 21 terms, and is equal to 953. Which prime, below
+one-million, can be written as the sum of the most consecutive primes?
 """
 from __future__ import print_function
-from utils import timer
+from utils import timer, MILL, take_upto, primes
 
 
-ANSWER = None
+ANSWER = 997651
 
 
 def test_answer():
@@ -24,10 +24,49 @@ def test_answer():
         assert ANSWER == main()
 
 
+PRIMES = list(take_upto(MILL, primes()))
+PRIME_SET = set(PRIMES)
+PRIME_SUMS = [PRIMES[0]]
+
+# Calculate the cumaltive sum of primes for each step.
+for psi, p in enumerate(PRIMES[1:]):
+    PRIME_SUMS.append(PRIME_SUMS[psi] + p)
+
 
 @timer
 def main():
-    pass
+    # We use a sliding window to calculate the longest run and its associated
+    # prime.
+    psl = len(PRIME_SUMS)
+    high_run = 0
+    high_prime = 0
+
+    for x in range(psl):
+        if psl - x < high_run:
+            print('break AAAA')
+            break
+
+        for y in range(psl - 1, x, -1):
+            diff = PRIME_SUMS[y] - PRIME_SUMS[x]
+            if diff > MILL:
+                continue
+
+            run_length = abs(x - y)
+            if run_length < high_run:
+                print('break BBBB')
+                break
+
+            if diff < high_prime:
+                print('break CCCC')
+                break
+
+            if diff in PRIME_SET:
+                if diff > high_prime:
+                    print('high_run:{} high_prime:{}'.format(run_length, diff))
+                    high_prime = diff
+                    high_run = run_length
+
+    return high_prime
 
 
 if __name__ == '__main__':

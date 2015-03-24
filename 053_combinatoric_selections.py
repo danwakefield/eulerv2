e7beb5bbec55f689bbf3a483653660dba7ada6e5
Daniel Wakefield <daniel.wakefield@hp.com>
2015-03-24 18:22:58 +0000

    Clean up Doctext

--- 053_combinatoric_selections.py
+++ 053_combinatoric_selections.py
@@ -3,12 +3,14 @@
 """
 https://projecteuler.net/problem=53
 
-There are exactly ten ways of selecting three from five, 12345: 123,
-124, 125, 134, 135, 145, 234, 235, 245, and 345 In combinatorics, we
-use the notation,^5 C_3 = 10. In general,^n C_r = n ! r !( n−r )!
-,where r ≤ n , n ! = n ×( n −1)×...×3×2×1, and 0! = 1. It is not until
-n = 23, that a value exceeds one-million:^23 C_10 = 1144066. How
-many, not necessarily distinct, values of^n C_r , for 1 ≤ n ≤ 100,
+There are exactly ten ways of selecting three from five, 12345:
+    123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+In combinatorics, we use the notation,^5 C_3 = 10.
+In general,^n C_r = n!r!(n−r)!, where r ≤ n, n! = n×(n−1)×...×3×2×1,
+and 0! = 1. It is not until n = 23,
+that a value exceeds one-million:
+    ^23 C_10 = 1144066.
+How many, not necessarily distinct, values of ^n C_r, for 1 ≤ n ≤ 100,
 are greater than one-million?
 
 Answer: 4075

af5d92b3807677cba91d2e6c3251471fbd53e92b
Daniel Wakefield <dwakefieldmail-git@yahoo.co.uk>
2015-03-29 21:16:11 +0100

    Fix question text for Q26

--- 026_reciprocal_cycles.py
+++ 026_reciprocal_cycles.py
@@ -5,11 +5,19 @@ https://projecteuler.net/problem=26
 
 A unit fraction contains 1 in the numerator. The decimal
 representation of the unit fractions with denominators 2 to 10 are
-given:^1 /_2 = 0.5^1 /_3 = 0.(3)^1 /_4 = 0.25^1 /_5 = 0.2^1 /
-_6 = 0.1(6)^1 /_7 = 0.(142857)^1 /_8 = 0.125^1 /_9 = 0.(1)^1 /
-_10 = 0.1 Where 0.1(6) means 0.166666..., and has a 1-digit recurring
-cycle. It can be seen that^1 /_7 has a 6-digit recurring cycle. Find
-the value of d < 1000 for which^1 /_d contains the longest recurring
+given:
+    1/2 = 0.5
+    1/3 = 0.(3)
+    1/4 = 0.25
+    1/5 = 0.2
+    1/6 = 0.1(6)
+    1/7 = 0.(142857)
+    1/8 = 0.125
+    1/9 = 0.(1)
+    1/10 = 0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring
+cycle. It can be seen that 1/7 has a 6-digit recurring cycle. Find
+the value of d < 1000 for which 1/d contains the longest recurring
 cycle in its decimal fraction part.
 """
 from __future__ import print_function
@@ -24,7 +32,6 @@ def test_answer():
         assert 0, 'Not Completed'
     else:
         assert ANSWER == main()
-
 
 
 @timer

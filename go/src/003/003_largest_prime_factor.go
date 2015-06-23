84d46a20a37c6b1c4652c528eb35af15d7ac9bae
Daniel Wakefield <dwakefieldmail-git@yahoo.co.uk>
2015-06-23 20:17:10 +0100

    Add Q3 for Go

--- go/src/003/003_largest_prime_factor.go
+++ go/src/003/003_largest_prime_factor.go
@@ -7,6 +7,24 @@ package main
 //
 // Answer: 6857
 
-func main() {
+import (
+	"fmt"
+	u "utils"
+)
 
+func main() {
+	N := 600851475143
+	limit := u.SqrtInt(N) + 1
+
+Outer:
+	for i := 2; i < limit; i++ {
+		for u.ModInt(N, i) == 0 {
+			N /= i
+
+			if N == 1 || N == i {
+				fmt.Println(i)
+				break Outer
+			}
+		}
+	}
 }
